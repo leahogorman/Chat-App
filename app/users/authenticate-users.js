6c40f2fd95c03009a8e82be727a7c1d62306f83f
Sajal <karkeesajal@gmail.com>
2020-11-03 09:13:42 -0800

    minor tweaks

--- app/users/authenticate-users.js
+++ app/users/authenticate-users.js
@@ -3,7 +3,7 @@
 
 var db = require('../config/connection');
 module.exports.authenticate=async function(user,password){
-    const result = await db.query('SELECT * FROM users WHERE username = ?',[user])
+    const result = await db.query('SELECT * FROM USERS WHERE username = ?',[user])
     console.log( ' ... database result: ', result )
     if( result.length<1 ){
         return { status: false, message: 'Sorry unknown user' }
